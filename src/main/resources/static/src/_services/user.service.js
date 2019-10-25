983ea59c395986130fcc289560d2900ac338dd55
user <4korobeynikov@gmail.com>
2019-10-25 07:36:41 +0300

    add React UI

--- src/main/resources/static/src/_services/user.service.js
+++ src/main/resources/static/src/_services/user.service.js
@@ -1,29 +1,49 @@
+import config from 'config';
+import sha256 from 'sha256';
+import { authHeader } from '../_helpers';
+
 export const userService = {
     login,
     logout,
     getAll
 };
 
-function login(username, password) {
+function login(login, password) {
+    let passHash = sha256(password);
     const requestOptions = {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password })
+//        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ login, passHash})
     };
 
-    return fetch(`${config.apiUrl}/users/authenticate`, requestOptions)
+    return fetch(`${config.apiUrl}/login?login=`+login+`&passHash=`+passHash, requestOptions)
         .then(handleResponse)
         .then(user => {
             // login successful if there's a user in the response
             if (user) {
-                // store user details and basic auth credentials in local storage
+                // store user details and basic auth credentials in local storage 
                 // to keep user logged in between page refreshes
-                user.authdata = window.btoa(username + ':' + password);
+                user.authdata = window.btoa(login + ':' + password);
                 localStorage.setItem('user', JSON.stringify(user));
+                return user;
             }
 
-            return user;
+
         });
+}
+
+function logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('user');
+}
+
+function getAll() {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/users`, requestOptions).then(handleResponse);
 }
 
 function handleResponse(response) {
